408766bea549c686ad1f9c150de7f58a5fa14ef3
BasemHennawi <60872972+BasemBahig@users.noreply.github.com>
2022-03-28 14:21:59 +0200

    trying to fix the form using CSS, and the server is fixed directly to the localhost

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 
 // Create a new date instance dynamically with JS
 const myDate = new Date();
-const newDate = myDate.getMonth()+ 1 + '.' +d.getDate()+'.'+d.getFullYear();
+const newDate = myDate.getMonth()+ 1 + '.' +myDate.getDate()+'.'+myDate.getFullYear();
 
 //Use provided API Key
-const APIKey=a177f8481c31fa96c3f95ad4f4f84610;
+const APIKey="a177f8481c31fa96c3f95ad4f4f84610";
 //const Base_URL=`https://api.darksky.net/forecast/[API_KEY]/[latitude], [longitude]`
 const Base_URL=`https://api.darksky.net/forecast/${APIKey}/[latitude], [longitude]`
 
 //add Eventlistener
 document.getElementById('button').addEventListener('click', getWeather);
 function getWeather(){
-    const addLocation=document.getElementById('location').value
+    const addLocation=document.getElementById('addLocation').value
     retrieveData(location)
     .then(function(data){
         if(addLocation!=null) {
