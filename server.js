c6f0e3367e7921f50f9c4bde9feb5ebac5f4dfd0
BasemHennawi <60872972+BasemBahig@users.noreply.github.com>
2022-03-28 11:36:03 +0200

    Fix Server Issue to run

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-import {express, req, res} from 'express'
-import cors from 'cors';
+const express = require('express');
 const app = express();
-app.use(corse());
+const cors = require('cors');
+
+app.use(express());
 const port = 4040;
 
-app.listening(port, (req,res)=>{
-console.log(`server is up and runing on localhost:${port}`);
+app.listen(port, (req,res)=>{
+console.log(`server is up and runing on http://localhost:${port}`);
 });
 
-export default app;
+//export default app;
